db954698301554e90af58513e55fa9b7c03e69cf
Dipesh_209 <prajapatdipesh86@gmail.com>
2023-12-16 19:55:15 +0530

    changes

--- Task - 2/app.js
+++ Task - 2/app.js
@@ -17,28 +17,6 @@ function fetchData() {
 }
 button.addEventListener("click", fetchData);
 
-// "id": 1,
-//         "name": "Leanne Graham",
-//         "username": "Bret",
-//         "email": "Sincere@april.biz",
-//         "address": {
-//             "street": "Kulas Light",
-//             "suite": "Apt. 556",
-//             "city": "Gwenborough",
-//             "zipcode": "92998-3874",
-//             "geo": {
-//                 "lat": "-37.3159",
-//                 "lng": "81.1496"
-//             }
-//         },
-//         "phone": "1-770-736-8031 x56442",
-//         "website": "hildegard.org",
-//         "company": {
-//             "name": "Romaguera-Crona",
-//             "catchPhrase": "Multi-layered client-server neural-net",
-//             "bs": "harness real-time e-markets"
-//         }
-
 function inputData(data) {
   let tableHead = document.createElement("thead");
 
@@ -107,9 +85,5 @@ function inputData(data) {
 
     tableBody.appendChild(tableHeadRow);
     table.appendChild(tableBody);
-
-    // let tableValues = document.createElement("p");
-    // tableValues.innerText = `${element.name}`;
-    // contentData.appendChild(tableValues);
   });
 }
